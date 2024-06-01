de663d5717243551beba86c35fceb2042c850427
Anantha Krishnan <142145233+ananthak803@users.noreply.github.com>
2024-06-01 07:00:47 +0000

    updated the popup message

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ const percentage = (attendedClasses/totalClasses)*100;
 let copy1 = totalClasses;
 let copy2 = attendedClasses;
 let temp =(copy2/copy1)*100;
+let maxClass = 0;
 while(temp<requiredPercentage){
     copy1+=1;
     copy2+=1;
@@ -15,7 +16,18 @@ while(temp<requiredPercentage){
 }
 const requiredClasses = copy2-attendedClasses;
     if(percentage >= requiredPercentage){
-        alert('you are safe');
+        let copy1 = totalClasses;
+        // let copy2 = attendedClasses;
+        let temp =(copy2/copy1)*100;
+        while(temp >=requiredPercentage){
+            copy1+=1;
+            temp=(attendedClasses/copy1)*100;
+            maxClass=copy1-totalClasses-1;
+        }
+        alert(`You are safe. You have ${Math.round(percentage)}%. You can bunk ${maxClass} classes`);
+        // document.querySelector(".attendancePercentage").innerHTML =`Attendance Percentage: ${Math.round(percentage)}% `;
+        // document.querySelector(".status").innerHTML=`Debarred Status: Safe`;
+        // document.querySelector(".extra").innerHTML =`Available Class Bunk: ${maxClass}`;
     }
     else{
         
@@ -25,6 +37,7 @@ const requiredClasses = copy2-attendedClasses;
             temp=(copy2/copy1)*100;
         }
         const requiredClasses = copy2-attendedClasses;
-        alert(`you are not safe and need to attend ${requiredClasses} more class`);
+        alert(`you are not safe.You have ${Math.round(percentage)}%. You need to attend ${requiredClasses} more class`);
+        // document.querySelector(".result").innerHTML =`You are not safe.You have ${Math.round(percentage)}% attendance. You need to attend ${requiredClasses} more class`;
     }
 }
